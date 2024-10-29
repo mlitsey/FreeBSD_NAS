8605b64df091cf07dcc797734619cda0d14cd113
mlitsey <19708932+mlitsey@users.noreply.github.com>
2024-10-29 12:56:58 -0400

    added files for aliases and tmux logging

---
+++ tmux-global-logging.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+# sudo apt install tmux
+#set -x # enables debugging
+#set -e # bash will exit on error
+#printf " \n\n Don't forget to check /var/log/syslog or /var/log/cron for cronjob script run times \n\n"
+
+
+if [ -n "$TMUX_PANE" ] && [ "$TMUX_PANE_LOGGING" != "1" ]; then
+  export TMUX_PANE_LOGGING=1
+else
+  exit 0
+fi
+
+LOGS=$HOME/.tmux/logs
+mkdir --parents $LOGS
+#LOG_PATH="$LOGS/$(date +%Y%m%d_%H%M%S).pane${TMUX_PANE//[^0-9]/}.log"
+#LOG_PATH="$LOGS/$(date +%F_%T).pane${TMUX_PANE//[^0-9]/}.log"
+LOG_PATH="$LOGS/$(date +%F_%H%M%S).pane${TMUX_PANE//[^0-9]/}.log"
+
+# sudo apt install ansifilter
+tmux pipe-pane -t "${TMUX_PANE}" "exec cat - | ansifilter >> $LOG_PATH"
+
+# add to .bashrc
+# $HOME/.tmux/tmux-global-logging.sh
